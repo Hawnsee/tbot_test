0d2525f29678d06ba045413764546ce5bc511176
Ismael Sánchez Gómez <45861899+Hawnsee@users.noreply.github.com>
2025-02-02 17:59:20 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7526382715:AAGnru5cfReKivOgD5CELQ_OHptCi9Hpl4A'; // Replace with your own bot token
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === '/start') {
+    bot.sendMessage(chatId, 'Welcome to the bot!');
+  }
+});
